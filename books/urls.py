3670cb85a66b7485036a6a0f13d1a9d4171a5b8f
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-02 22:28:26 +0300

    add app urls

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:genre_id>/', by_genre, name='by_genre'),
+    path('add/', add, name='add'),
+    path('delete/<int:book_id>/', delete, name='delete'),
+    path('edit/<int:book_id>/', edit, name='edit'),
 ]
