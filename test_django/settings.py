22af9c18d2146a63fa8e19a8638f688a64b89231
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-02 22:27:57 +0300

    add media urls

--- test_django/settings.py
+++ test_django/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'books.apps.BooksConfig',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
