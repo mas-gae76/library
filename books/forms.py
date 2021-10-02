cbc92f7896ec97876098f24b70fdcf9f86158e72
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-02 22:25:54 +0300

    add Book form

---
+++ books/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Books
+
+
+class BookForm(forms.ModelForm):
+
+    class Meta:
+        model = Books
+        fields = ('title', 'author', 'description', 'image', 'release_date', 'genre')
