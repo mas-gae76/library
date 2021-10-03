8515e2db8cf62e1324b69012864dbdc9431c7743
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-03 21:01:58 +0300

    edit serializers

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from ..models import Books, Author
+from ..models import Books, Author, Genre
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['initials']
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name']
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=True)
+    genre = GenreSerializer(many=True)
+
+    class Meta:
+        model = Books
+        fields = ['id', 'title', 'author', 'description', 'image', 'release_date', 'genre']
 
 
 class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=True)
+    genre = GenreSerializer(many=True)
+
     class Meta:
         model = Books
-        fields = ['id', 'title', 'author', 'description', 'image', 'release_date']
+        fields = ['title', 'author', 'genre']
