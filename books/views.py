8ce9e1c9b63d00f145e51c2fa5f69c547dbce974
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-03 14:32:38 +0300

    add ordering and filtering

--- books/views.py
+++ books/views.py
@@ -3,13 +3,32 @@ from .models import Books, Genre
 from .forms import BookForm
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from django.views.generic import ListView
+from django.db.models import Q
 
 
-def index(request):
-    books = Books.objects.all()
-    genres = Genre.objects.all()
-    context = {'bs': books, 'gs': genres}
-    return render(request, 'index.html', context)
+class MainView(ListView):
+    model = Books
+    paginate_by = 10
+    queryset = Books.objects.all()
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        filter = self.request.GET.get('filter')
+        if not filter:
+            context = Books.objects.all()
+        else:
+            query = Q(author__initials__icontains=filter)
+            context = Books.objects.filter(query)
+        order = self.request.GET.get('orderby', 'title')
+        new_context = context.order_by(order)
+        return new_context
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['gs'] = Genre.objects.all()
+        context['filter'] = self.request.GET.get('filter', '')
+        return context
 
 
 def by_genre(request, genre_id):
