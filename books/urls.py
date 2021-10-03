95fbd3534753d89f4c930730f843e82c39057101
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-03 16:48:36 +0300

    edit view urls

--- books/urls.py
+++ books/urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:genre_id>/', by_genre, name='by_genre'),
+    path('', MainView.as_view(), name='index'),
+    path('<int:genre_id>/', BookGenre.as_view(), name='by_genre'),
     path('add/', add, name='add'),
     path('delete/<int:book_id>/', delete, name='delete'),
     path('edit/<int:book_id>/', edit, name='edit'),
+    path('details/<int:book_id>/', details, name='details')
 ]
