f5f1876a2157b69a963ab3e3b8bb5f0504e588f0
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-04 16:30:39 +0300

    add alternative API views (comment)

--- books/api/views.py
+++ books/api/views.py
@@ -78,3 +78,23 @@ def delete(request, book_id):
         return HttpResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
     except Exception:
         return HttpResponse({'error': 'Что-то пошло не так'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+'''
+from .serializers import *
+from .serializers import Books
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListAPIView
+
+
+class APIBookList(ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+
+
+class APIBookDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookDetailSerializer
+
+    def get_queryset(self):
+        return Books.objects.filter(id=self.kwargs.get('pk'))
+'''
