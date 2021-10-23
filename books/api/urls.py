367f56495271ece81b3fb50866bb4e1dc70e4a3b
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-23 23:15:07 +0300

    changed API urls

--- books/api/urls.py
+++ books/api/urls.py
@@ -3,9 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('read/', get_books),
-    path('create/', add),
-    path('update/<int:book_id>', edit),
-    path('delete/<int:book_id>', delete),
-    path('details/<int:book_id>', get_book_detail),
+    path('', APIBookList.as_view()),
+    path('details/<int:pk>', APIBookDetail.as_view()),
 ]
