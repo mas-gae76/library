afcc53f3e72533a8094a9da5d49e75f0a6f11dac
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-23 22:42:37 +0300

    changed function controllers to classes

--- accounts/views.py
+++ accounts/views.py
@@ -1,52 +1,41 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import auth
 from django.shortcuts import redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from .forms import LoginForm, RegisterForm
-from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
-@csrf_exempt
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return redirect('profile')
-                else:
-                    return HttpResponse('Из этой учётной записи уже вышли')
-            else:
-                return HttpResponse('Вы ввели некорректный логин или пароль')
-    else:
-        form = LoginForm()
-    return render(request, 'registration/login.html', {'form': form})
+class UserLogin(LoginView):
+    template_name = 'registration/login.html'
+    authentication_form = LoginForm
+
+    def form_valid(self, form):
+        cd = form.cleaned_data
+        user = authenticate(username=cd['username'], password=cd['password'])
+        login(self.request, user)
+        return render(self.request, 'registration/profile.html')
 
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            login(request, user)
-            return render(request, 'registration/profile.html', {'form': form})
-    else:
-        form = RegisterForm()
-    return render(request, 'registration/signup.html', {'form': form})
+class UserRegisterView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = RegisterForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
 
 
 def logout_view(request):
     auth.logout(request)
-    return redirect('index')
+    return redirect('login')
 
 
-@csrf_exempt
+@login_required
 def show_person_cabinet(request):
     return render(request, 'registration/profile.html')
 
