e69975c6e88e506c89309ca75147c8ce67f97787
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-23 16:30:51 +0300

    changed function controllers to classes

--- books/views.py
+++ books/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Books, Genre
 from .forms import BookForm
-from django.shortcuts import get_object_or_404
-from django.contrib import messages
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.db.models import Q
 
 
@@ -37,49 +36,44 @@ class BookGenre(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return Books.objects.filter(genre=self.kwargs['genre_id'])
+        return Books.objects.filter(genre=self.kwargs['pk'])
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['cg'] = Genre.objects.get(pk=self.kwargs['genre_id']).name
+        context['cg'] = Genre.objects.get(pk=self.kwargs['pk']).name
         context['gs'] = Genre.objects.all()
         return context
 
 
-def details(request, book_id):
-    book = get_object_or_404(Books, pk=book_id)
-    return render(request, 'details.html', {'book': book})
+class BookDetailView(DetailView):
+    model = Books
+    template_name = 'details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetailView, self).get_context_data(**kwargs)
+        context['gs'] = Genre.objects.all()
+        return context
 
 
-def add(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST, request.FILES)
-        if form.is_valid():
-            if 'image' in request.FILES:
-                form.image = request.FILES['image']
-            form.save()
-            return redirect('index')
-        else:
-            return render(request, 'add.html', {'form': form})
-    else:
-        form = BookForm()
-        return render(request, 'add.html', {'form': form})
+class CreateBookView(CreateView):
+    form_class = BookForm
+    template_name = 'add.html'
+    success_url = '/books/details/{id}'
 
 
-def delete(request, book_id):
-    deleted_book = Books.objects.filter(id=book_id)
-    deleted_book.delete()
-    return redirect('index')
+class DeleteBookView(DeleteView):
+    model = Books
+    success_url = '/books/'
+    template_name = 'delete.html'
+
+    def get_queryset(self):
+        return Books.objects.filter(id=self.kwargs['pk'])
 
 
-def edit(request, book_id):
-    book = get_object_or_404(Books, pk=book_id)
-    if request.method == 'POST':
-        form = BookForm(request.POST, request.FILES, instance=book)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS, 'Объявление исправлено')
-            return redirect('index')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'edit.html', {'form': form})
+class UpdateBookView(UpdateView):
+    form_class = BookForm
+    template_name = 'edit.html'
+    success_url = '/books/details/{id}'
+
+    def get_queryset(self):
+        return Books.objects.filter(id=self.kwargs['pk'])
