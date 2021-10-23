e4a46780b525ce38a68d3135dd28480067e97b75
mas-gae76 <alex2904.goryachev@yandex.com>
2021-10-23 22:42:13 +0300

    changed urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url
 from . import views
 from django.urls import path
 
 
 urlpatterns = [
-    url('logout/', views.logout_view, name='logout'),
-    path('login/', views.user_login, name='login'),
-    url('signup/', views.register, name='signup'),
-    url('profile/', views.show_person_cabinet, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.UserLogin.as_view(), name='login'),
+    path('signup/', views.UserRegisterView.as_view(), name='signup'),
+    path('profile/', views.show_person_cabinet, name='profile'),
 ]
